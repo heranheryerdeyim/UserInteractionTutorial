241b987755b83397e4620a089f03f34e37ea6914
Nicholas Arner <nicholasarner@gmail.com>
2015-02-07 15:36:43 -0500

    Code cleanup

--- UserInteractionTutorial/NewInstrument.m
+++ UserInteractionTutorial/NewInstrument.m
@@ -20,35 +20,38 @@
         _frequencyValue = [[AKInstrumentProperty alloc] initWithValue:440
                                                               minimum:220
                                                               maximum:880];
+        [self addProperty:_frequencyValue];
+
         
         _carrierMultValue   = [[AKInstrumentProperty alloc] initWithValue:0.5
                                                                   minimum:0
                                                                   maximum:1];
+        [self addProperty:_carrierMultValue];
+
         
         _modIndexValue   = [[AKInstrumentProperty alloc] initWithValue:0.5
                                                                minimum:0
                                                                maximum:1];
+        [self addProperty:_modIndexValue];
+
         
         _amplitudeValue   = [[AKInstrumentProperty alloc] initWithValue:0.25
                                                                 minimum:0
                                                                 maximum:0.5];
         
-        [self addProperty:_frequencyValue];
-        [self addProperty:_carrierMultValue];
-        [self addProperty:_modIndexValue];
         [self addProperty:_amplitudeValue];
         
         // INSTRUMENT DEFINITION ===============================================
 
         AKFMOscillator *fmOscillator;
         fmOscillator = [[AKFMOscillator alloc] init];
-        [self connect:fmOscillator];
-        
         fmOscillator.baseFrequency     = _frequencyValue;
         fmOscillator.carrierMultiplier = _carrierMultValue;
         fmOscillator.modulationIndex   = _modIndexValue;
         fmOscillator.amplitude         = _amplitudeValue;
         
+        [self connect:fmOscillator];
+
         // AUDIO OUTPUT ========================================================
 
         AKAudioOutput *audioOutput = [[AKAudioOutput alloc] initWithAudioSource:fmOscillator];
